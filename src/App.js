3a367609e28b3e0de2e81fb15d7220e32b1a0a01
Naveen Verma <nveenverma@gmail.com>
2021-05-15 12:54:34 +0400

    Adding functionality to Sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import { Container } from 'react-bootstrap'
 
 import Header from './components/header/Header'
@@ -6,17 +7,23 @@ import Sidebar from "./components/sidebar/Sidebar"
 import HomeScreen from "./screens/homeScreen/HomeScreen"
 
 import "./_app.scss";
-
 const App = () => {
+
+    const [sidebar, toggleSidebar] = useState(false);
+
+    const handleToggleSidebar = () => toggleSidebar(value => !value);
+
     return (
         <>
-          <Header />  
+          <Header handleToggleSidebar={handleToggleSidebar}/>  
           <div className="app__container border border-info">
-              <Sidebar />
+              <Sidebar 
+                sidebar={sidebar} 
+                handleToggleSidebar={handleToggleSidebar}
+              />
               <Container fluid className="app__main border border-warning">
                   <HomeScreen />
               </Container>
-
           </div>
         </>
     )
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,10 +5,13 @@ import { FaBars } from "react-icons/fa"
 import { AiOutlineSearch } from "react-icons/ai"
 import {MdNotifications, MdApps } from "react-icons/md"
 
-const Header = () => {
+const Header = ({ handleToggleSidebar }) => {
     return (
         <div className="border border-dark header">
-            <FaBars className="header__menu" size={26} />
+            <FaBars 
+                className="header__menu" size={26} 
+                onClick={() => handleToggleSidebar()}    
+            />
             <img 
                 src="http://pngimg.com/uploads/youtube/youtube_PNG2.png"
                 alt=""
