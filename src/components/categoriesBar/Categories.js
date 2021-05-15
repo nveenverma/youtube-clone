b2997eaaab8fca2b44578e3cce1cb15eb8e96b82
Naveen Verma <nveenverma@gmail.com>
2021-05-15 18:56:05 +0400

    Design Categories Bar

--- src/components/categoriesBar/Categories.js
+++ src/components/categoriesBar/Categories.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './_categories.scss'
+
+const keywords = [
+    'All',
+    'React JS',
+    'Angular JS',
+    'React JS',
+    'Angular JS',
+    'React JS',
+    'Angular JS',
+    'React JS',
+    'Angular JS',
+    'React JS',
+    'Angular JS',
+    'React JS',
+    'Angular JS',
+    'React JS',
+    'Angular JS',
+    'React JS',
+    'Angular JS',
+]
 
 const Categories = () => {
+
+    const [activeElement, setActiveElement] = useState('All');
+    const handleClick = value => {
+        setActiveElement(value)
+    }
+
     return (
-        <div>
-            Bar
+        <div className="categoriesBar">
+            {keywords.map((value, i) => 
+                <span 
+                    className={activeElement === value ? 'active' : ''}
+                    key={i}
+                    onClick={() => handleClick(value)}
+                >
+                    {value}
+                </span>
+            )}
         </div>
     )
 }
