80f28153fff5d8645c4742c687d28fb7942dcdd0
Naveen Verma <nveenverma@gmail.com>
2021-05-15 05:42:56 +0400

    Design Sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,51 @@
 import React from 'react'
+import "./_sidebar.scss"
+
+import {
+    MdSubscriptions,
+    MdExitToApp,
+    MdHistory,
+    MdLibraryBooks,
+    MdHome,
+    MdSentimentDissatisfied,
+    MdThumbUp
+ } from "react-icons/md"
+
 
 const Sidebar = () => {
     return (
-        <div className="border border-danger">
-            Sidebar
-        </div>
+        <nav  className="border border-danger sidebar">
+            <li>
+                <MdHome size={23} />
+                <span>Home</span>
+            </li>
+            <li>
+                <MdSubscriptions size={23} />
+                <span>Subscriptions</span>
+            </li>
+            <li>
+                <MdThumbUp size={23} />
+                <span>Liked Videos</span>
+            </li>
+            <li>
+                <MdHistory size={23} />
+                <span>History</span>
+            </li>
+            <li>
+                <MdLibraryBooks size={23} />
+                <span>Library</span>
+            </li>
+            <li>
+                <MdSentimentDissatisfied size={23} />
+                <span>I don't know</span>
+            </li>
+            <hr />
+            <li>
+                <MdExitToApp size={23} />
+                <span>Log Out</span>
+            </li>
+            <hr />
+        </nav>
     )
 }
 
