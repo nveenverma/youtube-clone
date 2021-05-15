ebe8ad9d3e41d453307407dbf64c06ffd92a4c59
Naveen Verma <nveenverma@gmail.com>
2021-05-15 04:57:46 +0400

    Design Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import "./_header.scss"
+
+import { FaBars } from "react-icons/fa"
+import { AiOutlineSearch } from "react-icons/ai"
+import {MdNotifications, MdApps } from "react-icons/md"
 
 const Header = () => {
     return (
-        <div className="border border-dark">
-            Header
+        <div className="border border-dark header">
+            <FaBars className="header__menu" size={26} />
+            <img 
+                src="http://pngimg.com/uploads/youtube/youtube_PNG2.png"
+                alt=""
+                className="header__logo"
+            />
+            <form>
+                <input type="text" placeholder="search" />
+                <button>
+                    <AiOutlineSearch size={22} />
+                </button>
+            </form>
+
+            <div className="header__icons">
+                <MdNotifications size={28} />
+                <MdApps size={28} />
+                <img
+                    src="https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png"
+                    alt="avatar"
+                />
+            </div>
         </div>
     )
 }
